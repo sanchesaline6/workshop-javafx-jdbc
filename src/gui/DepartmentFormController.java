671edb5401a916da2d8e994b9c49a4b0a35846a6
Aline Sanches <sanches.aline6@gmail.com>
2019-09-28 17:16:30 -0300

    Saving a new Department

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,17 +3,26 @@ package gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import db.DbException;
+import gui.util.Alerts;
 import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable
 {
 	private Department department;
+	
+	private DepartmentService service;
 	
 	@FXML
 	private TextField txtId;
@@ -31,20 +40,53 @@ public class DepartmentFormController implements Initializable
 	private Button btCancel;
 	
 	@FXML
-	public void onBtSaveAction()
+	public void onBtSaveAction(ActionEvent event)
 	{
-		System.out.println("onBtSaveAction");
+		if(department == null)
+		{
+			throw new IllegalStateException("Entity was null");
+		}
+		
+		if(service == null)
+		{
+			throw new IllegalStateException("Service was null");
+		}
+		
+		try
+		{
+			department = getFormData();
+			service.saveOrUpdate(department);
+			Utils.currentStage(event).close();
+		}
+		catch(DbException e)
+		{
+			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
+		}
 	}
 	
-	@FXML
-	public void onBtCancelAction()
+	private Department getFormData() 
 	{
-		System.out.println("onBtCancelAction");
+		Department obj = new Department();
+		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		obj.setName(txtName.getText());
+		
+		return obj;
+	}
+
+	@FXML
+	public void onBtCancelAction(ActionEvent event)
+	{
+		Utils.currentStage(event).close();
 	}
 
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		initializeNodes();
+	}
+	
+	public void setDepartmentService (DepartmentService service)
+	{
+		this.service = service;
 	}
 	
 	public void setDepartment(Department department)
